413ad8ca1ecc97fd15f2cd8b07a7e86ad0c66a5c
FAHIMBHAI786 <100590475+FAHIMBHAI786@users.noreply.github.com>
2022-10-18 17:18:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import joblib
 
-model = joblib.load('spam-ham') # we are li=oding the pipeline model using joblib
-st.title('SPAM-HAM CLASSIFIER') # It creates a title in web app
-ip = st.text_input('Enter the message') # It create a text box in WEBAPP
+model = joblib.load('spam-ham')# we are loading the pipelined model using joblib
+st.title('SPAM-HAM CLASSIFIER')#It creates a title in webapp
+ip = st.text_input('Enter the message')#It creates a text box in the webapp
 op = model.predict([ip])
 if st.button('Predict'):
-  st.title(op[0])
+  st.title(op[0]) #st.button will create a button with the name Predict
+  #st.title(op[0]) # the 0th element in op variable is displayed as a title
 
 
